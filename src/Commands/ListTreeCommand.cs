e544ab9553104613f3fe35d2ae86b5bf94f233d5
DA <dragan.anatolie.m@gmail.com>
2025-10-02 22:02:02 +0300

    refactor ls-tree command

--- src/Commands/ListTreeCommand.cs
+++ src/Commands/ListTreeCommand.cs
@@ -1,11 +1,20 @@
 using System.IO.Compression;
-using System.Security.Cryptography;
 using System.Text;
 
 namespace codecrafters_git.Commands;
 
 public class ListTreeCommand : ICommand
 {
+    private record TreeEntry(string Type, string Hash, string Name) : IComparable<TreeEntry>
+    {
+        public int CompareTo(TreeEntry? entry) => 
+            string.Compare(Name, entry?.Name, StringComparison.OrdinalIgnoreCase);
+    }; 
+    
+    private const byte SpaceByte = (byte)' ';
+    private const byte NullByte = 0;
+    private const int ByteHashLength = 20;
+   
     public void Run(string[] args)
     {
         // tree <size>\0
@@ -15,70 +24,95 @@ public class ListTreeCommand : ICommand
         var flag = args[1];
         var hash = args[2];
         
-        var gitObjectHash = args[2];
-        var gitObjectDir = gitObjectHash[..2];
-        var gitObjectName = gitObjectHash[2..];
-        var gitObjectPath = $".git/objects/{gitObjectDir}/{gitObjectName}";
-        var outputBuilder = new StringBuilder();
-        var sortedOutput = new SortedDictionary<string, string>();
+        var path = GetObjectPath(hash);
+        var treeEntries = ReadStreamTreeEntries(path);
+        PrintTreeEntries(treeEntries, flag == "--name-only");
+    }
+
+    private SortedSet<TreeEntry> ReadStreamTreeEntries(string objPath)
+    {
+        var entriesSet = new SortedSet<TreeEntry>();
+        using var fs = File.OpenRead(objPath);
+        using var zlib = new ZLibStream(fs, CompressionMode.Decompress);
+        using var br = new BinaryReader(zlib);
+
+        SkipHeader(br);
+        while (TryReadStreamTreeEntry(br, out var treeEntry))
+            entriesSet.Add(treeEntry!);
         
+        return entriesSet;
+    }
+
+    private bool TryReadStreamTreeEntry(BinaryReader br, out TreeEntry? entry)
+    {
         try
         {
-            using var fs = File.OpenRead(gitObjectPath);
-            using var zlib = new ZLibStream(fs, CompressionMode.Decompress);
-            using var memStream = new MemoryStream();
-            zlib.CopyTo(memStream);
-            memStream.Position = 0;
-            using var reader = new BinaryReader(memStream);
-            while (reader.ReadByte() != 0); // skip the header part
-            while (reader.BaseStream.Position != reader.BaseStream.Length)
-            {
-                byte b;
-                
-                // read mode
-                var modeBytes = new List<byte> ();
-                while ((b = reader.ReadByte()) != (byte)' ')
-                    modeBytes.Add(b);
-                var modeChars = Encoding.ASCII.GetChars(modeBytes.ToArray());
-                var modeStr = new string(modeChars);
-
-                // read object name
-                var objectNameBytes = new List<byte> ();
-                while ((b = reader.ReadByte()) != 0)
-                    objectNameBytes.Add(b);
-                var objectNameChars = Encoding.ASCII.GetChars(objectNameBytes.ToArray());
-                var objectNameStr = new string(objectNameChars);
-                
-                // read object hash
-                var objectHashBuffer = reader.ReadBytes(20);
-                var objectHashStr = BitConverter.ToString(objectHashBuffer)
-                    .Replace("-", "")
-                    .ToLowerInvariant();
-
-                sortedOutput.Add(objectNameStr, $"{GetObjectType(modeStr)} {objectHashStr}\t{objectNameStr}{Environment.NewLine}");
-            }
-
-            foreach (var pair in sortedOutput)
-            {
-                if (args[1] == "--name-only")
-                    Console.WriteLine(pair.Key);
-                else
-                    Console.Write(pair.Value);
-            }
+            entry = ReadTreeEntry(br);
+            return true;
         }
-        catch (Exception e)
+        catch (EndOfStreamException)
         {
-            Console.WriteLine(e);
-            throw;
+            entry = null;
+            return false;
         }
     }
+
+    private TreeEntry ReadTreeEntry(BinaryReader br)
+    {
+        var objectType = ReadUntil(br, SpaceByte);
+        var objectName = ReadUntil(br, NullByte);
+        var objectHash = ReadHash(br);
+
+        return new TreeEntry(GetObjectType(objectType), objectHash, objectName);
+    }
+
+    private void SkipHeader(BinaryReader br)
+    {
+        while (br.ReadByte() != NullByte) ;
+    }
+
+    private string ReadUntil(BinaryReader br, byte delimiter)
+    {
+        byte b;
+        var bytesList = new List<byte>();   
+        while ((b = br.ReadByte()) != delimiter)
+            bytesList.Add(b);
+        
+        return Encoding.ASCII.GetString(bytesList.ToArray());
+    }
+
+    private string ReadHash(BinaryReader br)
+    {
+        var hashBytes = br.ReadBytes(ByteHashLength);
+        var hashHex = Convert.ToHexString(hashBytes).ToLowerInvariant();
+        return hashHex;
+    }
     
+    private string GetObjectPath(string objectHash)
+    {
+        var directory = objectHash[..2];
+        var fileName = objectHash[2..];
+        return $".git/objects/{directory}/{fileName}";
+    }
 
     private string GetObjectType(string mode) => mode switch
     {
-
         "40000" => "040000 tree",
         "100644" or "100755" or "120000" => $"{mode} blob",
-        _ => "wrong parsing"
+        _ => throw new InvalidOperationException($"Unknown mode {mode}")
     };
+    
+    private void PrintTreeEntries(SortedSet<TreeEntry> entries, bool nameOnly = false)
+    {
+        if (nameOnly)
+        {
+            foreach (var entry in entries)
+                Console.WriteLine(entry.Name);
+            
+            return;
+        }
+        
+        foreach (var entry in entries)
+            Console.WriteLine($"{entry.Type} {entry.Hash}\t{entry.Name}");
+    }
 }
